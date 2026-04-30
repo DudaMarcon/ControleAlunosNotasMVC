4d267c1481972c7ea183582ac58d0c5faec80ec7
Aluno <aluno@fatecitu.local>
2026-04-30 17:32:30 -0300

    add project files

---
+++ Models/Notas.cs
@@ -0,0 +1,13 @@
+﻿namespace Sistema_notaAluno.Models
+{
+    public class Notas
+    {
+        public int Id { get; set; }
+
+        public string Disciplina { get; set; }
+
+        public double Valor { get; set; }
+
+        public int AlunoId { get; set; }
+    }
+}
